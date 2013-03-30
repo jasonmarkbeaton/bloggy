587f2f4aa3886355f94b273c500db8aa6e0654c7
Parkerrr <jason@parkerrr.com>
2013-03-30 19:33:40 -0400

    Added form functions

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,7 @@ Bloggy::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  # Make sure to change in production
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
